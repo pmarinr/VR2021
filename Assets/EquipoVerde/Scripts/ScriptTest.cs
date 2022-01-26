39756adeae1fcc384e1c551c563a503f3d0d3d07
Pedro Marin <pedro@escav.es>
2022-01-26 18:06:31 +0100

    Espacio de nombres

---
+++ Assets/EquipoVerde/Scripts/ScriptTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VR2021.EquipoAzul;
+
+namespace VR2021.EquipoVerde
+{
+    public class ScriptTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+         
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
