48941dfd69b595612995fd1d64f1054a1c71db8a
Shad150 <cgpablo150@gmail.com>
2022-02-09 19:56:36 +0100

    Golf gaming
    Oa jiji

---
+++ Assets/EquipoAzul/MiniGolf/Scripts/ClubBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubBehaviour : MonoBehaviour
+{
+    private Rigidbody _rb;
+    [SerializeField] private GameObject _club;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Club"))
+        {
+            _rb.AddForce(_club.transform.position, ForceMode.Impulse);
+        }
+    }
+}
