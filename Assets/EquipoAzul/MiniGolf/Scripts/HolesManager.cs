12c65c95e954157592cabfc074e4a62d01069a7a
PablooCG <cgpablo150@gmail.com>
2022-03-09 13:51:06 +0100

    Golf Course done

--- Assets/EquipoAzul/MiniGolf/Scripts/HolesManager.cs
+++ Assets/EquipoAzul/MiniGolf/Scripts/HolesManager.cs
@@ -16,6 +16,7 @@ public class HolesManager : MonoBehaviour
     {
         if (other.CompareTag("Ball"))
         {
+            print("BallIN");
             _nextHole._completedHole = true;
         }
     }
--- Assets/EquipoAzul/MiniGolf/Scripts/NextHole.cs
+++ Assets/EquipoAzul/MiniGolf/Scripts/NextHole.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class NextHole : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _ball;
     [SerializeField] private Transform[] _holesGO;
     private int _currentHole;
     public bool _completedHole;
- 
-    void Start()
-    {
-    }
 
     void Update()
     {
         if (_completedHole)
         {
-            _completedHole = false;
+            
             StartCoroutine(NextHoleCR());
+            _completedHole = false;
         }
     }
 
-    private IEnumerable NextHoleCR()
+    private IEnumerator NextHoleCR()
     {
         yield return new WaitForSeconds(5f);
         _currentHole++;
+        print("BRBRRB" + _currentHole);
         _player.transform.position = _holesGO[_currentHole].transform.position;
+        _ball.transform.position = _holesGO[_currentHole].GetComponentInChildren<Transform>().position;
     }
 }
